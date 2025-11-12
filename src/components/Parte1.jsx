8400b3672bedd22a69223ae9fc79b2bdaba97acc
Kevin <kevin.ferreira0510@gmail.com>
2025-11-12 03:07:19 -0300

    commit final

--- src/components/Parte1.jsx
+++ src/components/Parte1.jsx
@@ -8,7 +8,7 @@ export default function Parte1() {
         <div className={styles.texto}>
           <h2>Nossa Loja - Guitars</h2>
           <p>
-            Se você é um apaixonado por música e busca um novo instrumento sem abrir mão da qualidade, 
+            Se você é um apaixonado por música e busca um novo instrumento sem abrir mão da qualidade, seja do som, material, cordas, 
             você está no lugar certo! Em nossa loja, você encontrará os melhores itens, como teclados, 
             pianos (digitais e acústicos), contrabaixos, baterias, guitarras, violões, instrumentos 
             de sopro e muito mais, todos com o selo de qualidade das melhores marcas do mercado para 
